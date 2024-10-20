7cd6ec72d3e86d85f0d985068d9028e13b99a066
Muciej <maciej.jozefkowicz@gmail.com>
2024-10-20 16:51:44 +0200

    Added basic time comparison between implementations

--- cppLibs/MD5Modified/ut/crossCheckTest.cpp
+++ cppLibs/MD5Modified/ut/crossCheckTest.cpp
@@ -2,7 +2,29 @@
 #include <MD5Downloaded/md5.hpp>
 #include <MD5Modified/md5.hpp>
 #include <string>
+#include <functional>
+#include <cstdint>
+#include <iostream>
 
+namespace
+{
+
+const auto repeats = 1000;
+
+template <typename F, typename... Args>
+int64_t measureTime(F&& func, Args&&... args)
+{
+    int64_t average = 0;
+    for(int i = 0; i < repeats; i++)
+    {
+        auto start = std::chrono::high_resolution_clock::now();
+        std::invoke(std::forward<F>(func), std::forward<Args>(args)...);
+        auto stop = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
+        average += duration.count() / repeats;
+    }
+    return average;
+}
 
 TEST(MD5CrossCheck, basicHash)
 {
@@ -12,3 +34,13 @@ TEST(MD5CrossCheck, basicHash)
     EXPECT_EQ(hashOrg, hashMod);
 }
 
+TEST(MD5CrossCheck, speedDifference)
+{
+    const std::string msg = "TestMsg";
+    const auto timeOrg = measureTime(MD5Downloaded::GetMD5String, msg);
+    const auto timeMod = measureTime(MD5Modified::GetMD5String, msg);
+    std::cout << "Original: " << timeOrg << std::endl;
+    std::cout << "Modified: " << timeMod << std::endl;
+}
+
+} // namespace
