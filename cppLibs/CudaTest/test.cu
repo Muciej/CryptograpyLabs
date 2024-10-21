7a4dffc73c0087d7dafc069538d3093a8f253927
Muciej <maciej.jozefkowicz@gmail.com>
2024-10-21 23:11:46 +0200

    Changed TestCuda type to library for testing purposes

--- cppLibs/CudaTest/test.cu
+++ cppLibs/CudaTest/test.cu
@@ -24,23 +24,37 @@ __global__ void whoami(void) {
     // printf("blockIdx.x: %d, blockIdx.y: %d, blockIdx.z: %d, threadIdx.x: %d, threadIdx.y: %d, threadIdx.z: %d\n", blockIdx.x, blockIdx.y, blockIdx.z, threadIdx.x, threadIdx.y, threadIdx.z);
 }
 
-int main(void)
+// int main(void)
+// {
+//     const int b_x = 100, b_y = 100, b_z = 200;
+//     const int t_x = 16, t_y = 16, t_z = 4; // the max warp size is 32, so 
+//     // we will get 2 warp of 32 threads per block
+
+//     int blocks_per_grid = b_x * b_y * b_z;
+//     int threads_per_block = t_x * t_y * t_z;
+
+//     printf("%d blocks/grid\n", blocks_per_grid);
+//     printf("%d threads/block\n", threads_per_block);
+//     printf("%d total threads\n", blocks_per_grid * threads_per_block);
+
+//     dim3 blocksPerGrid(b_x, b_y, b_z); // 3d cube of shape 2*3*4 = 24
+//     dim3 threadsPerBlock(t_x, t_y, t_z); // 3d cube of shape 4*4*4 = 64
+
+//     whoami<<<blocksPerGrid, threadsPerBlock>>>();
+//     cudaDeviceSynchronize();
+//     return 0;
+// }
+
+void invokeKernel()
 {
     const int b_x = 100, b_y = 100, b_z = 200;
     const int t_x = 16, t_y = 16, t_z = 4; // the max warp size is 32, so 
-    // we will get 2 warp of 32 threads per block
 
     int blocks_per_grid = b_x * b_y * b_z;
     int threads_per_block = t_x * t_y * t_z;
-
-    printf("%d blocks/grid\n", blocks_per_grid);
-    printf("%d threads/block\n", threads_per_block);
-    printf("%d total threads\n", blocks_per_grid * threads_per_block);
-
-    dim3 blocksPerGrid(b_x, b_y, b_z); // 3d cube of shape 2*3*4 = 24
-    dim3 threadsPerBlock(t_x, t_y, t_z); // 3d cube of shape 4*4*4 = 64
+    dim3 blocksPerGrid(b_x, b_y, b_z);
+    dim3 threadsPerBlock(t_x, t_y, t_z);
 
     whoami<<<blocksPerGrid, threadsPerBlock>>>();
     cudaDeviceSynchronize();
-    return 0;
 }
